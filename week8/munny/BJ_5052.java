b0f71b95f7f053a6308a1b294b3bf26008269ab6
sodp5 <sodp5@naver.com>
2020-07-21 15:28:05 +0900

    modify 5052

--- week8/munny/BJ_5052.java
+++ week8/munny/BJ_5052.java
@@ -3,6 +3,7 @@ package week_8;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BJ_5052 {
     public static void main(String[] args) {
@@ -39,7 +40,12 @@ public class BJ_5052 {
                     arr[n] = br.readLine();
                 }
 
-                Arrays.sort(arr);
+                Arrays.sort(arr, new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return o1.length() - o2.length();
+                    }
+                });
 
                 for (int n = 0; n < N; n++) {
                     num = arr[n];
